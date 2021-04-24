61ccda900baa7b56633c9704e57f46fc4e04b790
Luis Yanes <yanes.luis@gmail.com>
2021-04-25 00:37:14 +0100

    Initial version of main loop
    Prints values using the Hardware API

--- HardwareController/__main__.py
+++ HardwareController/__main__.py
@@ -1,10 +1,39 @@
 import click
-import HardwareController
+from time import sleep
+from . import VERSION
+from .BME280 import BME280
+from .TSL2561 import TSL2561
+
+class Chamber:
+    def __init__(self, id):
+        self.id = id
+
+        # self.actuators = hardwareActuators()
+        self.sensors = [BME280(), TSL2561()]
+        # self.registered_sensors = getChamberSensors(self.id)
+        # self.registered_actuators = getChamberActuators(self.id)
+
 
 @click.command()
 @click.option('--api_endpoint', default='http://localhost:5000/api')
-def main(api_endpoint):
-    print(f"GC_hardware - {HardwareController.VERSION}")
+@click.option('--chamber_id', default=1)
+def main(api_endpoint, chamber_id):
+    print(f"GC_hardware - {VERSION}")
+    print("Press CTRL-C to terminate")
+    running = True
+
+    try:
+        chamber = Chamber(chamber_id) # TODO: Collect *chamber_id* configuration
+        while running:
+            print("Measuring: ")
+            for s in chamber.sensors:
+                for measure_type in s.measures():
+                    measurement = s.measure(measure_type)
+                    print(f"{measure_type}: {measurement.value:.2f}")
+            print("")
+            sleep(2) # 10s
+    except KeyboardInterrupt:
+        print("Terminating...")
 
 if __name__ == "__main__":
     main()
